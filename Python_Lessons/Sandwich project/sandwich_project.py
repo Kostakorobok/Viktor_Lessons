f90aa3a5a5c22126948bd5990d5c424a79777910
Constantine <193867275+Kostakorobok@users.noreply.github.com>
2025-01-19 16:39:15 +1100

    -Made a start on Sanwich project

---
+++ Python_Lessons/Sandwich project/sandwich_project.py
@@ -0,0 +1,19 @@
+logo = """
+ ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▄▄▄ ▗▖ ▗▖▗▄▄▄▖ ▗▄▄▖▗▖ ▗▖     ▗▄▖ ▗▄▄▖ ▗▄▄▄ ▗▄▄▄▖▗▄▄▖  ▗▄▄▖
+▐▌   ▐▌ ▐▌▐▛▚▖▐▌▐▌  █▐▌ ▐▌  █  ▐▌   ▐▌ ▐▌    ▐▌ ▐▌▐▌ ▐▌▐▌  █▐▌   ▐▌ ▐▌▐▌   
+ ▝▀▚▖▐▛▀▜▌▐▌ ▝▜▌▐▌  █▐▌ ▐▌  █  ▐▌   ▐▛▀▜▌    ▐▌ ▐▌▐▛▀▚▖▐▌  █▐▛▀▀▘▐▛▀▚▖ ▝▀▚▖
+▗▄▄▞▘▐▌ ▐▌▐▌  ▐▌▐▙▄▄▀▐▙█▟▌▗▄█▄▖▝▚▄▄▖▐▌ ▐▌    ▝▚▄▞▘▐▌ ▐▌▐▙▄▄▀▐▙▄▄▖▐▌ ▐▌▗▄▄▞▘
+                                                                          
+"""
+
+print(logo)
+
+sandwich_orders = ["The Zesty Zeppelin", "Cheddar Avalanche", "Midnight Muncher", "Crispy Wanderer", "The Italian Stroll", "Smoky Sunrise", "Savory Stackhouse", "The Pickled Paladin", "Garden Crunch Deluxe", "The Meaty Maestro", "Avocado Blisswich", "Spicy Stardust", "The Roaming Reuben", "Golden Gobbler", "The Sourdough Sailor", "Sweet Heat Harmony", "Bacon Bliss Bomb", "The Grilled Titan", "Pesto Powerhouse", "The Double Decker Delight"]
+finished_sanwiches = []
+
+for i in sandwich_orders:
+    print(f"I've made your {i}")
+    finished_sanwiches_temp = i
+    finished_sanwiches.append(finished_sanwiches_temp)
+
+print(finished_sanwiches)
