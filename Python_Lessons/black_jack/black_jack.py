83a694d9183257a635eb2bcaa8cb05a06179c311
Constantine <193867275+Kostakorobok@users.noreply.github.com>
2025-01-31 16:15:22 +1100

    black jack

--- Python_Lessons/black_jack/black_jack.py
+++ Python_Lessons/black_jack/black_jack.py
@@ -16,7 +16,7 @@ main = True
 game_on = True
 
 def dev_code_stop():
-    input("press Enter to continue")
+    input("\npress Enter to continue\n")
 
 def ft_dealing_cards(deck, player):
     for i in range (2):
@@ -62,18 +62,21 @@ while main:
                 print(f"\nAI's cards are: {ai_cards}\nTotal: {ai_cards_total}")
 
                 if user_cards_total == 21 and ai_cards_total != 21:
-                    print("Black Jack! You win!")
+                    print("\nBlack Jack! You win!")
+                    draw_card = False
                     game_on = False
                     dev_code_stop()
                 elif ai_cards_total == 21 and user_cards_total != 21:
-                    print("Black Jack! You loose, AI wins!")
+                    print("\nBlack Jack! You loose, AI wins!")
+                    draw_card = False
                     game_on = False
                 elif user_cards_total == 21 and ai_cards_total == 21:
-                    print("Double Black Jack! Draw!")
+                    print("\nDouble Black Jack! Draw!")
+                    draw_card = False
                     game_on = False
                 else:  
                     if user_cards_total > 21:
-                        print("User score is over 21\nChecking if have Ace...\n")  
+                        print("\nUser score is over 21\nChecking if have Ace...\n")  
                         dev_code_stop()
                         for i in range(len(user_cards)):
                             if user_cards[i] == 11:
@@ -83,33 +86,72 @@ while main:
                                 user_cards_total = ft_card_sum(user_cards, user_cards_total)
 
                                 if user_cards_total > 21:
+                                    draw_card = False
                                     game_on = False
-                                    print("You have ACE but still loose!")
+                                    print("\nYou have ACE but still loose!")
                                     dev_code_stop()
+                                    break
                                 else:
                                     user_cards[i] = 11
-                                    print("After changing Value of of Ace from 11 to 1, your score is now lower than 21.\nGame carries on.")
+                                    print("\nAfter changing Value of of Ace from 11 to 1, your score is now lower than 21.\nGame carries on.")
                                     dev_code_stop()
                                 break
                             else:
                                 break
                             
                         if 11 not in user_cards:
-                            game_on = False
                             draw_card = False
-                            print("You dont have an Ace. You loose!")       
+                            game_on = False
+                            print("\nYou dont have an Ace. You loose!")       
                             dev_code_stop()
+                            break
                 
-                    print(f"User's curent Hand: {user_cards}")
+                    print(f"\nUser's curent Hand: {user_cards}")
 
-                    draw_card_choice = input("Draw another card?\n yes/no\n").lower()
+                    draw_card_choice = input("\nDraw another card?\n yes/no\n").lower()
                     if draw_card_choice == "yes":
                         ft_add_card(deck0, user_cards)
-                        print(f"You drew another card. Your current hand is: {user_cards}")
+                        print(f"\nYou drew another card. Your current hand is: {user_cards}")
                         dev_code_stop()
                     elif draw_card_choice == "no":
+                        print(f"\nYou've choosen not to draw card. Your current hand is: {user_cards}")
+                        print(f"AI's current hand is: {ai_cards}\nAIs term:")
                         dev_code_stop()
-                        pass
+
+                        if ai_cards_total < 17:
+                            print(f"AIs total score is {ai_cards_total}, which is less than 17, thus drawing another card:")
+                            dev_code_stop
+                            ft_add_card(deck0, ai_cards)
+                            ai_cards_total = ft_card_sum(ai_cards, ai_cards_total)
+                            if ai_cards_total > 21:
+                                draw_card = False
+                                game_on = False
+                                print(f"AI's new hand: {ai_cards}\nAI's new total: {ai_cards_total}")
+                                print("AI wins!")
+                                dev_code_stop()
+                                break
+                        
+                        else:
+                            ai_cards_total = ft_card_sum(ai_cards, ai_cards_total)
+                            print(f"User hand: {user_cards} | User score: {user_cards_total}")
+                            print(f"AI's hand: {ai_cards}   | AI's score: {ai_cards_total}")
+
+                            if ai_cards_total > user_cards_total:
+                                draw_card = False
+                                game_on = False
+                                print("AI Wins!")
+                                break
+                            elif ai_cards_total < user_cards_total:
+                                draw_card = False
+                                game_on = False
+                                print("User wins")
+                                break
+                            else:
+                                draw_card = False
+                                game_on = False
+                                print("Draw!")
+                                break
+
                     else:
                         print("\nWrong input, try again.\n")
 
